cf4dd87d1d23a645b3225de4bae51f79ec30dbfb
Cameron White <cameronwhite4121@gmail.com>
2025-01-30 19:32:53 -0800

    Created databasehandler class with table creation statements

---
+++ MyVirtualBookshelf/Models/DatabaseHandler.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVirtualBookshelf.Models
+{
+    internal class DatabaseHandler
+    {
+        private SQLiteConnection _db;
+
+        public DatabaseHandler()
+        {
+
+            _db = new SQLiteConnection("path/to/your/database");
+            _db.CreateTable<Shelf>();
+            _db.CreateTable<Book>();
+            _db.CreateTable<ShelfContents>();
+        }
+
+    }
+}
