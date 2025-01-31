7bd47dcf29f2a4a43f52d38a3d543803157eb5fa
Cameron White <cameronwhite4121@gmail.com>
2025-01-30 19:07:15 -0800

    Added relations between the two main classes so they can be joined

--- MyVirtualBookshelf/Models/ShelfContents.cs
+++ MyVirtualBookshelf/Models/ShelfContents.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#nullable disable
 
 namespace MyVirtualBookshelf.Models
 {
-    internal class ShelfContents
+    [Table("ShelfContents")]
+    public class ShelfContents
     {
+        /// <summary>
+        /// Foreign key to Shelf class
+        /// </summary>
+        public int ShelfId { get; set; }
+
+        /// <summary>
+        /// Foreign key to Book class
+        /// </summary>
+        public string Isbn { get; set; }
+
+        /// <summary>
+        /// Tracks how many of a specific book are in a specific shelf
+        /// </summary>
+        [Column("Quantity")]
+        public int Quantity { get; set; }
+
     }
 }
